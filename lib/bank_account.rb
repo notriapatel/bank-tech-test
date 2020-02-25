579ff23751c715e99b79cd39df587825127b5acc
ria patel <ria.amy.patel@gmail.com>
2020-02-25 17:06:29 +0000

    method added to return reverse chron array of transaction strings, transaction formatting added

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,17 +1,28 @@
 class BankAccount
   attr_reader :balance
 
-  def initialize
+  def initialize(printer = StatementPrinter.new)
     @balance = 0
+    @transactions = []
+    @date = DateTime.now.strftime('%d/%m/%Y')
   end
 
   def deposit(amount)
     raise 'Invalid amount' if amount.negative?
+    @credit = amount
     @balance += amount
+    @transactions.push("#{@date} || #{format('%.2f', @credit)} || || #{format('%.2f', @balance)}")
   end
 
   def withdraw(amount)
     raise 'Insufficient funds' if (@balance - amount).negative?
+    @debit = amount
     @balance -= amount
+    @transactions.push("#{@date} || || #{format('%.2f', @debit)} || #{format('%.2f', @balance)}")
   end
+
+  def sort_transactions
+    @transactions.reverse
+  end
+
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,4 +1,5 @@
 require 'bank_account'
+require 'timecop'
 
 describe BankAccount do
   let(:account) { BankAccount.new }
@@ -27,6 +28,15 @@ describe BankAccount do
 
     it 'raises an error if withdrawal takes balance below 0' do
       expect { account.withdraw(10) }.to raise_error 'Insufficient funds'
+    end
+  end
+
+  describe '#transactions' do
+    it 'returns a sorted array of formatted transactions' do
+      Timecop.freeze(Time.local(2020, 2, 24))
+      account.deposit(10)
+      account.withdraw(5)
+      expect(account.sort_transactions).to eq ['24/02/2020 || || 5.00 || 5.00', '24/02/2020 || 10.00 || || 10.00']
     end
   end
 end
