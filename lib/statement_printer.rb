3cfd4e3f8deea703fd69f76f4fa2dba096b4b3a0
ria patel <ria.amy.patel@gmail.com>
2020-02-26 15:09:17 +0000

    extracting statement formatting to separate class

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,5 +1,6 @@
 class StatementPrinter
-  def print_statement
-    'date || credit || debit || balance'
+  def print_statement(transactions)
+    transaction_string = transactions.reverse.join(" \n ")
+    "date || credit || debit || balance \n " + transaction_string
   end
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -2,8 +2,18 @@ require 'statement_printer'
 
 describe StatementPrinter do
   let(:printer) { StatementPrinter.new }
+  let(:transactions) do
+    [
+      '24/02/2020 || 5.00 || || 5.00',
+      '25/02/2020 || || 1.00 || 1.00'
+    ]
+  end
 
   it 'prints the title line first' do
-    expect(printer.print_statement).to eq 'date || credit || debit || balance'
+    expect(printer.print_statement([])).to eq "date || credit || debit || balance \n "
+  end
+
+  it 'adds a transaction to the statement' do
+    expect(printer.print_statement(transactions)).to eq "date || credit || debit || balance \n 25/02/2020 || || 1.00 || 1.00 \n 24/02/2020 || 5.00 || || 5.00"
   end
 end
